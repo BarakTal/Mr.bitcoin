5c40d612360511a79a015060d3bf67117ff95d66
Tal Barak <tal63566@gmail.com>
2019-08-04 15:42:35 +0300

    Updates

---
+++ precache-manifest.5475cea84a849291c487ff84a89e7a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af46c4dd0a6aa8d889f1d9d5f960f989",
+    "url": "/Mr.bitcoin/index.html"
+  },
+  {
+    "revision": "d3876a889674953fbbef",
+    "url": "/Mr.bitcoin/static/css/main.577b7a38.chunk.css"
+  },
+  {
+    "revision": "922f462cc9e8b14d4537",
+    "url": "/Mr.bitcoin/static/js/2.7accc24c.chunk.js"
+  },
+  {
+    "revision": "d3876a889674953fbbef",
+    "url": "/Mr.bitcoin/static/js/main.8ea75919.chunk.js"
+  },
+  {
+    "revision": "8e7e846c1e2317e1b324",
+    "url": "/Mr.bitcoin/static/js/runtime~main.1969a104.js"
+  }
+]);
